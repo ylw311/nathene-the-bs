5cfcdee78b40c558b4e056c353774ac8ccbe8b94
Linda Wang <61102300+ylw311@users.noreply.github.com>
2023-08-13 00:57:25 -0400

    cleaned up some logic

--- index.ts
+++ index.ts
@@ -19,8 +19,8 @@ app.listen(PORT, () => console.log(`TypeScript Battlesnake Server listening at h
 function handleIndex(request: Request, response: Response<SnakeInfo>) {
     const battlesnakeInfo: SnakeInfo = {
         apiversion: '1',
-        author: 'Nathene',
-        "color": "#F8C8DC",
+        author: 'ylw311',
+        "color": "#E37383",
         "head": "snowman",
         "tail": "comet"
     }
@@ -40,20 +40,15 @@ function handleMove(request: GameRequest, response: Response<Move>) {
     const body = gameData.you.body;
     const board = gameData.board;
     const snakes = gameData.board.snakes;
-    console.log("snakes" + JSON.stringify(snakes, null, 2));
-    console.log("body" + JSON.stringify(body, null, 2));
+    // console.log("snakes" + JSON.stringify(snakes, null, 2));
+    // console.log("body" + JSON.stringify(body, null, 2));
 
     // const possibleMoves: Direction[] = ['up', 'down', 'left', 'right']
     // const move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
 
 
 
-
-        // floodfill to prevent it locking itself 
-        const claimed: boolean[][] = Array.from({ length: board.height }, () => new Array(board.width).fill(false));
-        floodFill(board, head, claimed);
-
-
+    
     // find closest food
     const closestFood = board.food.reduce((closest: any, food: any) => {
         const distance = Math.abs(food.x - head.x) + Math.abs(food.y - head.y);
@@ -68,14 +63,25 @@ function handleMove(request: GameRequest, response: Response<Move>) {
     }, null);
 
 
+     
+    // // Check if another snake is closer to the food
+    // problem: if there's several snakes, they could all be closer to the food than you
+    // const otherSnakeCloser = snakes.some(snake => {
+    //     if (snake.id === gameData.you.id) return false; // Skip own snake
+
+    //     const snakeHead = snake.body[0];
+    //     const snakeDistance = Math.abs(snakeHead.x - closestFood.food.x) + Math.abs(snakeHead.y - closestFood.food.y);
+    //     return snakeDistance < closestFood.distance;
+    // });
+
 
     const moves: Direction[]  = ['up', 'down', 'left', 'right']
   
-
+    
     let possibleCells = moves.map(move => {
         let dx = 0;
         let dy = 0;
-
+    
         if (move === 'up') {
             dy = 1;
         } else if (move === 'down') {
@@ -85,48 +91,74 @@ function handleMove(request: GameRequest, response: Response<Move>) {
         } else if (move === 'right') {
             dx = 1;
         }
-
-        return {
+    
+        // Calculate the new cell position after the move
+        const newCell = {
             x: head.x + dx,
             y: head.y + dy,
             direction: move,
+        };
+    
+        // Check if the new cell is out of bounds
+        if (
+            newCell.x >= 0 && newCell.x < board.width &&
+            newCell.y >= 0 && newCell.y < board.height
+        ) {
+            // Check if the new cell is not occupied by other snakes
+            const isSafe = !snakes.some((snake: { body: any[] }) =>
+                snake.body.some((piece: { x: number; y: number }) =>
+                    piece.x === newCell.x && piece.y === newCell.y
+                )
+            );
+    
+            // If the move is safe according to flood-fill, return the cell
+            // Otherwise, return null
+            return isSafe ? newCell : null;
         }
-    }).filter(direction => {
-        // dont go out of bounds
-        return direction.x >= 0 && direction.x < board.width && direction.y >= 0 && direction.y < board.height;
-    }).filter(direction => {
+    
+        return undefined;
+    }).filter((cell): cell is { x: number; y: number; direction: Direction } => cell !== null && cell !== undefined);
+    // Filter out null cells
+    
+    // Now, possibleCells contains only safe and valid moves
+    
+    
+    // Create a copy of possibleCells 
+    let possibleCellsWOAbsDeath = [...possibleCells]; 
 
-        // dont hit other snakes
-        return !snakes.some((snake: { body: any[] }) => snake.body.some((piece: { x: number; y: number }) => piece.x === direction.x && piece.y === direction.y));
-    });
-
-    
-    
-    
     if (possibleCells.length !== 1) {
-       console.log("more than 1 possible cell");
-       possibleCells = possibleCells.filter(direction => {
-            // Avoid cells that another snake's head could reach in one step
+        console.log("more than 1 possible cell");
+    
+      
+    
+        // Filter out cells that another snake's head could reach in one step
+        possibleCells = possibleCells.filter((cell: any) => {
+            console.log("cell" + JSON.stringify(cell, null, 2));
+            if (cell === undefined) {
+                return false;
+            }
+    
             const isAdjacentToOtherHead = snakes.some((snake: { id: string, body: any[] }) => {
-              
-                // check if the other snake's is longer (i.e. needs to be avoided)
-                if (snake.id !== gameData.you.id && snake.body.length >= body.length) { 
-                    
-                    return isAdjacentToHead(snake.body[0], direction.x, direction.y);
-                
+                if (snake.id !== gameData.you.id && snake.body.length >= body.length) {
+                    return isAdjacentToHead(snake.body[0], cell.x, cell.y);
                 }
                 return false;
             });
-            console.log("isAdjacentToOtherHeadDirection " + direction.direction + " " + isAdjacentToOtherHead)
-            
+    
+            console.log("isAdjacentToOtherHeadDirection " + cell.direction + " " + isAdjacentToOtherHead);
+    
+            // Also check if the move is safe according to flood-fill
             return !isAdjacentToOtherHead;
         });
     }
     
-   
-    
-   console.log(possibleCells);
+    console.log("possibleCells" + JSON.stringify(possibleCells, null, 2));
 
+    
+    // ... Rest of the code
+    
+
+    
 
 //    filter(direction => {
         // dont hit body piece 
@@ -140,59 +172,32 @@ function handleMove(request: GameRequest, response: Response<Move>) {
     let shortestPathLength = Infinity;
 
     for (const cell of possibleCells) {
-        const pathToFood = aStarSearch(board, cell, closestFood.food);
-        if (pathToFood && pathToFood.length < shortestPathLength) {
-            shortestPathLength = pathToFood.length;
-            bestMove = cell.direction as Direction;
+        if (cell) { // Add this null check
+            const pathToFood = aStarSearch(board, cell, closestFood.food);
+            if (pathToFood && pathToFood.length < shortestPathLength) {
+                shortestPathLength = pathToFood.length;
+                bestMove = cell.direction as Direction;
+            }
         }
     }
+    
+    
 
     console.log("best move: " + bestMove);
 
-
-
-
-    let move = bestMove || chooseSafeMove(board, head, body, possibleCells, claimed); // Implement your safe move logic
-  
-  
-  
+    let move : Direction | undefined = undefined;
     
-    // // Check if another snake is closer to the food
-    // const otherSnakeCloser = snakes.some(snake => {
-    //     if (snake.id === gameData.you.id) return false; // Skip own snake
-
-    //     const snakeHead = snake.body[0];
-    //     const snakeDistance = Math.abs(snakeHead.x - closestFood.food.x) + Math.abs(snakeHead.y - closestFood.food.y);
-    //     return snakeDistance < closestFood.distance;
-    // });
-
-    // // Choose a move based on A* pathfinding if no other snake is closer to the food
-    // // Otherwise, choose a safe move using flood-fill or other logic
-    // if (!otherSnakeCloser && pathToFood && pathToFood.length > 1) {
-    //     const nextCell = pathToFood[1];
-    //     move = getMoveDirection(head, nextCell);
-    // } else {
-    //     move = chooseSafeMove(board, head, body); // Implement your safe move logic
-    // }
-
-
-    // // move towards closest food
-    // const move = possibleCells.reduce((move: any, cell: any) => {
-    //     const distance = Math.abs(cell.x - closestFood.food.x) + Math.abs(cell.y - closestFood.food.y);
-    //     if (!move || distance < move.distance) {
-    //         return {
-    //             distance: distance,
-    //             move: cell.direction,
-    //         }
-    //     } else {
-    //         return move;
-    //     }
-    // }, null).move;
-
+    move = bestMove;
+  
+   
+   
     if (move === undefined) {
-        move = 'down';
-        console.log('bruh how you even get here')
+        // No optimal path, pick a random move from those that wont result in absolute death
+        // e.g. two moves could both lead to headvshead collision, but you wouldn't know what other snake picks (i.e. gg gl at this point)
+        const randomIndex = Math.floor(Math.random() * possibleCells.length);
+        move = possibleCellsWOAbsDeath[randomIndex]?.direction || 'down'; // Pick a random move or default to 'down'
     }
+
     
     console.log('MOVE: ' + move)
     response.status(200).send({
@@ -211,69 +216,6 @@ function isAdjacentToHead(head: { x: number; y: number }, x: number, y: number):
     return (
         Math.abs(head.x - x) <= 1 && Math.abs(head.y - y) <= 1
     );
-}
-
-
-// Helper function to determine the direction from current position to next position
-function getMoveDirection(current: { x: number; y: number }, next: { x: number; y: number }): Direction {
-    if (next.x > current.x) return 'right';
-    if (next.x < current.x) return 'left';
-    if (next.y > current.y) return 'down';
-    if (next.y < current.y) return 'up';
-    return 'up'; // Default fallback
-}
-
-function chooseSafeMove(board: any, head: { x: number; y: number }, body: any[], possibleCells: { x: number; y: number; direction: Direction }[], claimed: boolean[][]): Direction | undefined {
-    let bestMove: Direction | undefined = undefined;
-    let maxPotentialSpace = -1;
-
-    for (const cell of possibleCells) {
-        const potentialSpace = calculatePotentialSpace(board, head, body, cell, claimed); // Pass claimed array to the function
-        if (potentialSpace > maxPotentialSpace) {
-            maxPotentialSpace = potentialSpace;
-            bestMove = cell.direction;
-        }
-    }
-
-    return bestMove;
-}
-
-function calculatePotentialSpace(board: any, head: { x: number; y: number }, body: any[], cell: { x: number; y: number; direction: Direction }, claimed: boolean[][]): number {
-    if (!claimed[cell.y][cell.x]) {
-        return 0;
-    }
-    
-    const nextPos = cell;
-    const visited: boolean[][] = Array.from({ length: board.height }, () => new Array(board.width).fill(false));
-    let potentialSpace = 0;
-
-    const dfs = (x: number, y: number) => {
-        visited[y][x] = true;
-        potentialSpace++;
-
-        const neighbors = [
-            { x: x + 1, y },
-            { x: x - 1, y },
-            { x, y: y + 1 },
-            { x, y: y - 1 },
-        ];
-
-        for (const neighbor of neighbors) {
-            if (
-                neighbor.x >= 0 && neighbor.x < board.width &&
-                neighbor.y >= 0 && neighbor.y < board.height &&
-                !visited[neighbor.y][neighbor.x] &&
-                !body.some(segment => segment.x === neighbor.x && segment.y === neighbor.y) &&
-                !board.snakes.some((snake: { body: any[] }) => snake.body.some(segment => segment.x === neighbor.x && segment.y === neighbor.y))
-            ) {
-                dfs(neighbor.x, neighbor.y);
-            }
-        }
-    };
-
-    dfs(nextPos.x, nextPos.y);
-
-    return potentialSpace;
 }
 
 // Define a node class to represent nodes in the search
@@ -318,7 +260,6 @@ function aStarSearch(board: any, start: { x: number; y: number }, goal: { x: num
             if (
                 neighbor.x >= 0 && neighbor.x < board.width &&
                 neighbor.y >= 0 && neighbor.y < board.height &&
-                !board.snakes.some((snake: { body: any[] }) => snake.body.some(segment => segment.x === neighbor.x && segment.y === neighbor.y)) &&
                 !closedList.some(node => node.x === neighbor.x && node.y === neighbor.y)
             ) {
                 const g = current.g + 1;
@@ -340,35 +281,3 @@ function aStarSearch(board: any, start: { x: number; y: number }, goal: { x: num
 
 
 
-function floodFill(board: any, start: { x: number; y: number }, claimed: boolean[][]) {
-    const queue: { x: number; y: number }[] = [start];
-    claimed[start.y][start.x] = true;
-
-    while (queue.length > 0) {
-        const current = queue.shift();
-
-        if (current !== undefined) {
-
-        const neighbors = [
-            { x: current.x + 1, y: current.y },
-            { x: current.x - 1, y: current.y },
-            { x: current.x, y: current.y + 1 },
-            { x: current.x, y: current.y - 1 },
-        ];
-    
-
-        for (const neighbor of neighbors) {
-            if (
-                neighbor.x >= 0 && neighbor.x < board.width &&
-                neighbor.y >= 0 && neighbor.y < board.height &&
-                !claimed[neighbor.y][neighbor.x] &&
-                !board.snakes.some((snake: { body: any[] }) => snake.body.some(segment => segment.x === neighbor.x && segment.y === neighbor.y))
-            ) {
-                claimed[neighbor.y][neighbor.x] = true;
-                queue.push(neighbor);
-            }
-        }
-    } 
-    
-    }
-}
